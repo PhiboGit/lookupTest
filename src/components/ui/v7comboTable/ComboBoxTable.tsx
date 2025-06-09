33197cb73d41d24197346c30cd7bfa56a1d62e34
phibo <phibo96@gmail.com>
2025-06-09 22:46:28 +0200

    global search

--- src/components/ui/v7comboTable/ComboBoxTable.tsx
+++ src/components/ui/v7comboTable/ComboBoxTable.tsx
@@ -1,4 +1,4 @@
-import { useComboBox } from "react-aria"
+import { useButton, useComboBox } from "react-aria"
 import { useComboBoxState, type ComboBoxStateOptions } from "react-stately"
 import { useMemo, useRef, useState } from "react"
 import { Popover } from "./Popover"
@@ -6,25 +6,27 @@ import { ComboButton } from "./ComboButton"
 import { ListBoxTable } from "./ListBoxTable"
 import {
   type ColumnDef,
-  type ColumnFiltersState,
   getCoreRowModel,
   getFilteredRowModel,
   getSortedRowModel,
   useReactTable,
   type SortingState,
 } from "@tanstack/react-table"
-import { Input } from "../input" // Assuming you have a custom Input component
+import { Input } from "../input"
+import { Button } from "../button"
 
 interface ComboBoxTableProps<T extends object = object>
   extends ComboBoxStateOptions<T> {
   items: Iterable<T>
   columns: ColumnDef<T>[]
 }
+
 export function ComboBoxTable<T extends { id: string | number }>(
   props: ComboBoxTableProps<T>
 ) {
   const [sorting, setSorting] = useState<SortingState>([])
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+  const [globalFilter, setGlobalFilter] = useState("")
+
   const { columns, items } = props
   const data = useMemo(() => Array.from(items || []), [items])
 
@@ -34,31 +36,28 @@ export function ComboBoxTable<T extends { id: string | number }>(
     getRowId: (row) => String(row.id),
     state: {
       sorting,
-      columnFilters,
+      globalFilter,
     },
     onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
   })
 
-  // Important for filtering: Process the items before passing them to the ComboBox
-  // This is for a client side filter/search
   const processedItems = useMemo(() => {
     return table.getRowModel().rows.map((row) => row.original)
   }, [table.getRowModel().rows])
 
   const state = useComboBoxState({
     onOpenChange: (isOpen) => {
-      // When the combobox is opened, clear the column filters to show all items
+      // When the combobox is opened, clear the filter to show all items
       if (isOpen) {
-        setColumnFilters([])
+        setGlobalFilter("")
       }
     },
     onInputChange: (value) => {
-      // When the user types, update the filter for the 'name' column
-      setColumnFilters([{ id: "name", value }])
+      setGlobalFilter(value)
     },
     ...props,
     items: processedItems,
@@ -69,7 +68,12 @@ export function ComboBoxTable<T extends { id: string | number }>(
   const listBoxRef = useRef<HTMLDivElement>(null)
   const popoverRef = useRef<HTMLDivElement>(null)
 
-  const { buttonProps, inputProps, listBoxProps, labelProps } = useComboBox(
+  const {
+    buttonProps: comboButtonProps,
+    inputProps,
+    listBoxProps,
+    labelProps,
+  } = useComboBox(
     {
       ...props,
       inputRef,
@@ -79,7 +83,7 @@ export function ComboBoxTable<T extends { id: string | number }>(
     },
     state
   )
-
+  const { buttonProps } = useButton(comboButtonProps, buttonRef)
   return (
     <div style={{ display: "inline-flex", flexDirection: "column" }}>
       <label {...labelProps}>{props.label}</label>
@@ -94,15 +98,11 @@ export function ComboBoxTable<T extends { id: string | number }>(
             flex: 1,
           }}
         />
-        <ComboButton
-          {...buttonProps}
-          buttonRef={buttonRef}
-          style={{ height: 24 }}
-        >
+        <Button {...buttonProps} ref={buttonRef} style={{ height: 24 }}>
           <span aria-hidden="true" style={{ padding: "0 2px" }}>
             ▼
           </span>
-        </ComboButton>
+        </Button>
       </div>
       {state.isOpen && (
         <Popover
